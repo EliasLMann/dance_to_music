61c56b4e0ddeafdaf024ec59bfb5017705b9cbac
EliasLMann <eliaslmann@gmail.com>
2024-04-24 17:18:40 -0400

    emotion regressor complete

--- music_to_emo/src/multi_task_regressor.py
+++ music_to_emo/src/multi_task_regressor.py
@@ -10,32 +10,46 @@ def r_squared(predictions, targets):
     r2 = 1 - ss_res / ss_tot
     return r2
 
-class EmotionRegressor(pl.LightningModule):
+class MultiTaskRegressor(pl.LightningModule):
     def __init__(self):
         super().__init__()
 
         self.lstm = nn.LSTM(input_size=128, hidden_size=64, num_layers=4,dropout=0.25, batch_first=True)
-
-        self.fc_layers = nn.Sequential(
+        self.relu = nn.ReLU()
+        # Separate FC layers for valence
+        self.fc_valence = nn.Sequential(
             nn.Linear(64, 32),
             nn.ReLU(),
             nn.Dropout(p=0.25),
             nn.Linear(32, 8),
             nn.ReLU(),
-            nn.Dropout(p=0.25),
-            nn.Linear(8, 2),
-            nn.Tanh()
+            nn.Linear(8, 1),
+            nn.Tanh()  # Assuming valence is a score between -1 and 1
         )
+        
+        # Separate FC layers for arousal
+        self.fc_arousal = nn.Sequential(
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Dropout(p=0.25),
+            nn.Linear(32, 8),
+            nn.ReLU(),
+            nn.Linear(8, 1),
+            nn.Tanh()  # Assuming arousal is a score between -1 and 1
+        )
+
 
     def forward(self, x):
         batch_size, seq_length, _ = x.shape
         x, _ = self.lstm(x)
+
         #fully connected layers applied for each time step
         x = x.contiguous().view(-1, x.shape[2])
-        x = self.fc_layers(x)
+        x_valence = self.fc_valence(x).view(batch_size, seq_length, -1)
+        x_arousal = self.fc_arousal(x).view(batch_size, seq_length, -1)
         #reshape to original shape
-        x = x.view(batch_size, seq_length, -1)
-        return x
+        out = torch.cat((x_valence, x_arousal), dim=-1)
+        return out
     
     def training_step(self, batch, batch_idx):
         features, valence_labels, arousal_labels = batch
